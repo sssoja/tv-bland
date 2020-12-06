47662a1465f219e2af536e435e12ca5fe3d6a597
sssoja <sophie@sssoja.com>
2020-12-06 20:22:48 +0100

    WIP

--- components/Rating.tsx
+++ components/Rating.tsx
@@ -1,9 +1,10 @@
-import { TypographyProps, typography } from "styled-system";
+import { SpaceProps, TypographyProps, space, typography } from "styled-system";
 
 import Flex from "./styles/Flex";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 interface ShowProps {
   rating: { average: number | null };
@@ -13,17 +14,28 @@ const NoRating = styled.p<TypographyProps>`
   ${typography};
 `;
 
-const NumberRating = styled.p<TypographyProps>`
+const NumberRating = styled.p<TypographyProps & SpaceProps>`
   ${typography};
+  ${space};
 `;
 
 const Rating = (props: ShowProps) => {
+  const location = useRouter();
+  const hideRating = "/";
+
+  if (hideRating.includes(location.pathname)) {
+    return null;
+  }
   if (props.rating.average === null) {
     return <NoRating fontSize={1}>No rating</NoRating>;
   }
+
   const rating = Math.round(props.rating.average / 2);
   const maxRating = 5;
   const emptyStars = maxRating - rating;
+
+  // TO DO all ratings equal to zero display as an integer not 0.0
+  // const zero = rating === 0 ? Math.round(zero) : average.toFixed(1);
 
   const renderFullStars = () => {
     return rating !== 0
@@ -50,10 +62,13 @@ const Rating = (props: ShowProps) => {
   };
 
   return (
-    <Flex mr={4} flexDirection="row">
+    <Flex flexDirection="row">
+      {" "}
       <span>{renderFullStars()}</span>
       <span>{renderEmptyStars()}</span>
-      <NumberRating fontSize={1}>{rating} / 5</NumberRating>
+      <NumberRating fontSize={1} mx={2}>
+        {rating} / 5
+      </NumberRating>
     </Flex>
   );
 };
