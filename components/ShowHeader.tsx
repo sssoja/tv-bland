a6680b035d5f5554a224b12bbc1e5b534565100c
sssoja <sophie@sssoja.com>
2020-12-06 23:35:21 +0100

    fixes link to homepage

--- components/ShowHeader.tsx
+++ components/ShowHeader.tsx
@@ -26,7 +26,7 @@ const ShowTitle = styled.h2<TypographyProps & SpaceProps>`
   ${space};
 `;
 
-const ShowLink = styled.a`
+const ShowImageContainer = styled.div`
   width: fit-content;
 `;
 
@@ -39,27 +39,31 @@ interface ShowHeaderProps {
 
 const ShowHeader = (props: ShowHeaderProps) => {
   return (
-    <Padding px={[3, 6, 6, 6]} pb={[0, 0, 0, 6]}>
-      <Flex flexDirection={["column", "column", "column", "row"]}>
+    <Flex
+      px={[3, 6, 6, 6]}
+      pb={[0, 0, 0, 6]}
+      flexDirection={["column", "column", "column", "row"]}
+    >
+      <ShowImageContainer>
         <Link href="/">
-          <ShowLink>
+          <a>
             <ShowImage src={props.image} maxHeight={[420, 420, 300, 300]} />
-          </ShowLink>
+          </a>
         </Link>
-        <Flex flexDirection="column" my={4} p={[0, 0, 0, 4]}>
-          <Flex flexDirection={["column", "row", "row", "row"]}>
-            <Rating rating={props.rating} showNumbers />
-          </Flex>
-          <ShowTitle fontSize={4} my={4}>
-            {props.name}
-          </ShowTitle>
-          <ShowSummary
-            fontSize={2}
-            dangerouslySetInnerHTML={{ __html: props.summary }}
-          />
+      </ShowImageContainer>
+      <Flex flexDirection="column" my={4} p={[0, 0, 0, 4]}>
+        <Flex flexDirection={["column", "row", "row", "row"]}>
+          <Rating rating={props.rating} showNumbers />
         </Flex>
+        <ShowTitle fontSize={4} my={4}>
+          {props.name}
+        </ShowTitle>
+        <ShowSummary
+          fontSize={2}
+          dangerouslySetInnerHTML={{ __html: props.summary }}
+        />
       </Flex>
-    </Padding>
+    </Flex>
   );
 };
 
